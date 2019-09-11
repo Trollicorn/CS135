2f439d4d223613dcd2f7a50d13436c77e05cfc8b
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2019-09-11 12:29:38 -0400

    lab 4f done, checks for valid sizes

--- labs/lab4/f/trapezoid.cpp
+++ labs/lab4/f/trapezoid.cpp
@@ -7,10 +7,15 @@ int main(){
 	std::cin >> width;
 	std::cout << "input height: ";
 	std::cin >> height;
-	
-	std::cout << "shape:\n";
 
-	int stars = width;
+	if (2*height > width){
+		std::cout << "\nimposible shape\n";
+		return 1;
+	}	
+	
+	std::cout << "\nshape:\n";
+
+
 	for (int r = 0; r < height; r++){
 		for (int c = 0; c < width; c++){
 			std::cout << (c >= r && c <= width - r - 1? "*" : " ");
