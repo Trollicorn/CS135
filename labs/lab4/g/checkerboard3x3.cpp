0e9f4909acb570ec4b993ae1a881a0da166c4383
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2019-09-11 12:32:45 -0400

    lab 4g: works halfway

---
+++ labs/lab4/g/checkerboard3x3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(){
+
+	int width, height;
+	std::cout << "input width: ";
+	std::cin >> width;
+	std::cout << "input height: ";
+	std::cin >> height;
+	
+	std::cout << "\nshape:\n";
+
+	for (int r = 0; r < height; r++){
+		for (int c = 0; c < width; c++){
+			std::cout << (r % 6 < 3 && c % 6 < 3? "*" : " ");
+		}
+
+		std::cout << "\n";
+	}
+
+	return 0;
+
+}
