6f7b6d2eface873c4572761e981c2f0eb48bac80
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2019-09-11 12:34:34 -0400

    did lab 4g

--- labs/lab4/g/checkerboard3x3.cpp
+++ labs/lab4/g/checkerboard3x3.cpp
@@ -12,7 +12,7 @@ int main(){
 
 	for (int r = 0; r < height; r++){
 		for (int c = 0; c < width; c++){
-			std::cout << (r % 6 < 3 && c % 6 < 3? "*" : " ");
+			std::cout << (r % 6 < 3 && c % 6 < 3 || r % 6 > 2 && c % 6 > 2 ? "*" : " ");
 		}
 
 		std::cout << "\n";
