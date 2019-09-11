d61a34d771f52ebb290310087b32fe876211e3b1
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2019-09-11 12:13:51 -0400

    did lab 4d and 4e (theyre basically the same thing)

---
+++ labs/lab4/d/lower.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+	int size;
+	std::cout << "input side length: ";
+	std::cin >> size;
+	
+	std::cout << "shape:\n";
+
+	for (int r = 0; r < size; r++){
+		for (int c = 0; c < size; c++){
+			std::cout << (c <= r ? "*" : " ");
+		}
+		std::cout << "\n";
+	}
+
+	return 0;
+
+}
