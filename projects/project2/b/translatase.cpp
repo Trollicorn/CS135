2ed6795af83a1fe0da8b7d4c083e99504b1a69cb
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2019-09-12 21:20:51 -0400

    added makefile and converted codons to proteins

--- projects/project2/b/translatase.cpp
+++ projects/project2/b/translatase.cpp
@@ -20,37 +20,48 @@ std::string dictionary_read(std::ifstream &dict, std::string codon){
 }
 
 int main(){
-	std::ifstream file("codons.tsv");
-	if (file.fail()){
+	std::ifstream codons("codons.tsv");
+	if (codons.fail()){
 		std::cerr << "file not found\n";
 		exit(1);
 	}
-	/*
+	
 	std::ifstream file("dna2b.txt");
 	if (file.fail()){
 		std::cerr << "file not found\n";
 		exit(1);
 	}
-	*/
-	std::cout << "GGU, expecting Gly: " << dictionary_read(file,"GGU") << '\n';
-	std::cout << "UAA, expecting Stop: " << dictionary_read(file,"UAA") << '\n';
-	std::cout << "MUG, expecting ERR: " << dictionary_read(file,"MUG") << '\n'; 
-	
-
 	
 	/*
+	std::cout << "GGU, expecting Gly: " << dictionary_read(codons,"GGU") << '\n';
+	std::cout << "UAA, expecting Stop: " << dictionary_read(codons,"UAA") << '\n';
+	std::cout << "MUG, expecting ERR: " << dictionary_read(codons,"MUG") << '\n'; 
+	*/
+
 	std::string strand;
 	while(getline(file,strand)){
 		//std::cout << strand << "\n";
+		
 		int len = strand.length();
+		/*
 		char transcript[len+1];
 		transcript[len] = '\0';
 		for (int i = 0; i < len; i++){
 			transcript[i] = complement(std::toupper(strand[i]));
 		}
 		std::cout << transcript << "\n";
+		*/
+		for (int i = 0; i < len; i += 3){
+			char codon[4];
+			for (int j = 0; j < 3; j++){
+				codon[j] = complement(strand[i+j]);
+			}
+			codon[4] = '\0';
+			std::cout << dictionary_read(codons, codon) << ' ';
+		}
+		std::cout << '\n';
 	}
-	*/
+
 	file.close();
 	return 0;
 }
