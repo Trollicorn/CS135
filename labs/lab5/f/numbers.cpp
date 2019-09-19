7fb9edd7f120d576e47e84d91a1bdc3490a46b92
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2019-09-19 11:20:48 -0400

    did nexttwinprime

---
+++ labs/lab5/f/numbers.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+bool isDivisibleBy(int n, int d){
+	return !(n % d);
+}
+
+bool isPrime(int n){
+	if (n == 2){
+		return true;
+	}
+	for (int i = 2; i < n; i++){
+		if (! (n%i)){
+			return false;
+		}
+	}
+	return true;
+}
+
+int nextPrime(int n){
+	int next = n+1;
+	while (true){
+		if (isPrime(next)){
+			return next;
+		}
+		next++;
+	}
+}
+
+int countPrimes(int a, int b){
+	int c = 0;
+	for (int i = a; i <= b; i++){
+		if (isPrime(i)){
+			c++;
+		}
+	}
+	return c;
+}
+
+bool isTwinPrime(int n){
+	return isPrime(n-2) || isPrime(n+2);
+}
+
+int nextTwinPrime(int n){
+	for (int i = n+1; true; i++){
+		if (isTwinPrime(i)){
+			return i;
+		}
+	}
+}
+
+int main(){
+	std::cout << nextTwinPrime(5) << '\n';
+	std::cout << nextTwinPrime(17) << '\n';
+	std::cout << nextTwinPrime(23) << '\n';
+}
