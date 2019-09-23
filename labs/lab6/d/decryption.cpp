d55b953d252c85625075f67570e682df13650a5f
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2019-09-23 10:49:31 -0400

    6d complete

--- labs/lab6/d/decryption.cpp
+++ labs/lab6/d/decryption.cpp
@@ -5,8 +5,8 @@ char shiftChar(char c, int rshift){
 	return (((int)c - a + rshift) % 26) + a;
 }
 
-char shiftChar(char c, char shift){
-	int n = shift - 'a';
+char shiftChar(char c, char shift, int decrypt){
+	int n = decrypt ? 'z' - shift + 1 : shift - 'a';
 	char a = isupper(c) ? 'A': 'a';
 	return ((int)c - a + n)% 26 + a;
 }
@@ -21,13 +21,17 @@ std::string encryptCaesar(std::string plaintext, int rshift){
 	return str;
 }
 
-std::string encryptVigenere(std::string plaintext, std::string keyword){
+std::string decryptCaesar(std::string ciphertext, int rshift){
+	return encryptCaesar(ciphertext, 26 - rshift);
+}
+
+std::string vigenere(std::string plaintext, std::string keyword, int decrypt){
 	int keyin = 0;
 	char crypt[plaintext.length()+1];
 	for (int i = 0; i < plaintext.length(); i++){
 		char p = plaintext[i];
 		if (isalpha(p)){
-			crypt[i] = shiftChar(p,keyword[keyin]);
+			crypt[i] = shiftChar(p,keyword[keyin],decrypt);
 			keyin = (keyin + 1) % keyword.length();
 		}else{
 			crypt[i] = p;
@@ -36,6 +40,13 @@ std::string encryptVigenere(std::string plaintext, std::string keyword){
 	crypt[plaintext.length()] = '\0';
 	std::string str(crypt);
 	return str;
+}
+std::string encryptVigenere(std::string plaintext, std::string keyword){
+	return vigenere(plaintext, keyword, 0);
+}
+
+std::string decryptVigenere(std::string ciphertext, std::string keyword){
+	return vigenere(ciphertext, keyword, 1);
 }
 
 int main(){
@@ -49,7 +60,10 @@ int main(){
 	int rshift;
 	std::cin >> rshift;
 	std::cout << "ciphertext     : ";
-	std::cout << encryptCaesar(plaintext,rshift) << '\n';
+	std::string caesarText = encryptCaesar(plaintext,rshift);
+	std::cout << caesarText << '\n';
+	std::cout << "decrypted      : ";
+	std::cout << decryptCaesar(caesarText,rshift) << '\n';
 
 	std::cout << '\n' << '\n';
 	std::cout << "= Vigenere =\n";
@@ -57,7 +71,10 @@ int main(){
 	std::string keyword;
 	std::cin >> keyword;
 	std::cout << "ciphertext     : ";
-	std::cout << encryptVigenere(plaintext,keyword) << '\n';
+	std::string vigenereText = encryptVigenere(plaintext,keyword);
+	std::cout << vigenereText << '\n';
+	std::cout << "decrypted      : ";
+	std::cout << decryptVigenere(vigenereText,keyword) << '\n';
 	return 0;
 
 
