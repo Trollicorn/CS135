dc90b29223a30cc89a7750c2c7fa4cf781a5c4e1
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2019-10-10 12:56:30 -0400

    did project 2 phase 2

--- projects/project2/pronounce.cpp
+++ projects/project2/pronounce.cpp
@@ -28,24 +28,54 @@ std::string pronounce(std::string input){
 		std::cerr << "file not found\n";
 		exit(1);
 	}
-	for (int i = 0; i < input.length(); i++){
-		input[i] = toupper(input[i]);
-	}
 	std::string line;
 	std::string word;
 	std::string pronunciation;
 	while (std::getline(file,line)){
 		splitOnSpace(line,word,pronunciation);
 		if (!input.compare(word)){
+			//std::string temp;
+			//splitOnSpace(pronunciation,word,pronunciation);
 			return pronunciation;
 		}
 	}
 	return "word not found";
 }
 
+void identical(std::string input, std::string phenomes){
+	std::ifstream file("cmudict.0.7a");
+	if (file.fail()){
+		std::cerr << "file not found\n";
+		exit(1);
+	}	
+	std::string line;
+	std::string word;
+	std::string pronunciation;
+	while (std::getline(file,line)){
+		splitOnSpace(line,word,pronunciation);
+		if (!phenomes.compare(pronunciation) && input.compare(word)){
+			std::cout << word << ' ';
+		}
+	}
+	
+}
+
 int main(){
-	std::cout << "word: locomotive\n";
-	std::cout << pronounce("locomotive") << '\n';
+	std::string word;
+	std::cin >> word;
+	for (int i = 0; i < word.length(); i++){
+		word[i] = toupper(word[i]);
+	}
+	std::cout << "Word             : " << word << '\n';
+	std::string pronunciation = pronounce(word);
+	if (!pronunciation.compare("word not found")){
+		std::cerr << "ERROR: WORD NOT FOUND\n";
+		exit(1);
+	}
+	std::cout << "Pronunciation    :" << pronunciation << '\n';
+	std::cout << "Identical        : ";
+	identical(word,pronunciation);
+	std::cout << '\n';
 }
 
 
