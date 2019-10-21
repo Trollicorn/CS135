9a1c0241d7695d5beef51117cbfdc8b46d93169f
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2019-10-21 12:38:21 -0400

    did exercise 6.8

---
+++ exercises/e6/6-8.cpp
@@ -0,0 +1,12 @@
+//Mohammed Uddin
+bool equals(int a[], int a_size, int b[], int b_size){
+	if (a_size != b_size){
+		return false;
+	}
+	for (int i = 0; i < a_size; i++){
+		if (a[i]!=b[i]){
+			return false;
+		}
+	}
+	return true;
+}
