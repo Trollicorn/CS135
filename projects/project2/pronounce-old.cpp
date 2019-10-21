fcd144c525dddd397564483bef0742fe8ee25acc
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2019-10-21 12:38:41 -0400

    recoding and optimizing project 2

--- projects/project2/pronounce-old.cpp
+++ projects/project2/pronounce-old.cpp
@@ -5,8 +5,7 @@ Instructor: Maryash
 Assignment: Project 2
 
 takes a word and outputs its pronunciation as a list of phonemes
-also outputs words with identical phonemes
-and words with 1 phoneme replaced, 1 phoneme inserted, or 1 phoneme removed
+also outputs words with identical phonemes and words with 1 phoneme replaced, 1 phoneme inserted, or 1 phoneme removed
 */
 
 #include <iostream>
@@ -82,54 +81,7 @@ void identical(std::string input, std::string phonemes){
 
 }
 
-bool checkReplace(std::string input,std::string pronunciation){
-	std::string before;
-	std::string after;
-	int words = numWords(input);
-	if (words != numWords(pronunciation)){
-		return false;
-	}
-	for (int i = 0; i < words; i++){
-		std::string pre0 = "", pre1 = "";
-		std::string before0, after0; //input
-		std::string before1, after1; //pronunciation
-		splitOnSpace(input,before0,after0); //first phoneme gone
-		splitOnSpace(pronunciation,before1,after1);
-		for (int j = 0; j < i; j++){ //remove other phonemes as necessary
-			pre0 = pre0 + before0 + " ";
-			pre1 = pre1 + before1 + " ";
-			splitOnSpace(after0,before0,after0);
-			splitOnSpace(after1,before1,after1);
-		}
-		if (!pre0.compare(pre1) && !after0.compare(after1)){
-			return true;
-		}
-	}
-	return false;
-}
-
 void replace(std::string input){
-	std::ifstream file("cmudict.0.7a");
-	if (file.fail()){
-		std::cerr << "file not found\n";
-		exit(1);
-	}
-	file.clear();
-	file.seekg(std::ios::beg);
-	std::string line;
-	while(std::getline(file,line)){
-		std::string word;
-		std::string pronunciation;
-		std::string junk;
-		splitOnSpace(line,word,pronunciation);
-		splitOnSpace(pronunciation,junk,pronunciation);//phonemes only, no space
-		if (checkReplace(input,pronunciation) && input.compare(pronunciation)){
-			std::cout << ' ' << word;
-		}
-	}
-}
-
-void replaceOld(std::string input){
 	std::ifstream file("cmudict.0.7a");
 	if (file.fail()){
 		std::cerr << "file not found\n";
@@ -170,54 +122,7 @@ void replaceOld(std::string input){
 	}
 }
 
-bool checkInsert(std::string input,std::string pronunciation){
-	std::string before;
-	std::string after;
-	int words = numWords(input);
-	if (words != numWords(pronunciation)-1){
-		return false;
-	}
-	for (int i = 0; i < words+1; i++){
-		std::string pre0 = "", pre1 = "";
-		std::string before0, after0 = input; //input
-		std::string before1, after1 = pronunciation; //pronunciation
-		for (int j = 0; j < i; j++){
-			splitOnSpace(after0,before0,after0); //move words from after to pre
-			splitOnSpace(after1,before1,after1); //remove words from pronunciation
-			pre0 = pre0 + before0 + " ";
-			pre1 = pre1 + before1 + " ";
-		}
-		splitOnSpace(after1,before1,after1); //remove first word
-
-		if (!pre0.compare(pre1) && !after0.compare(after1)){
-			return true;
-		}
-	}
-	return false;
-}
-
 void insert(std::string input){
-	std::ifstream file("cmudict.0.7a");
-	if (file.fail()){
-		std::cerr << "file not found\n";
-		exit(1);
-	}
-	file.clear();
-	file.seekg(std::ios::beg);
-	std::string line;
-	while(std::getline(file,line)){
-		std::string word;
-		std::string pronunciation;
-		std::string junk;
-		splitOnSpace(line,word,pronunciation);
-		splitOnSpace(pronunciation,junk,pronunciation);//phonemes only, no space
-		if (checkInsert(input,pronunciation)){
-			std::cout << ' ' << word;
-		}
-	}
-}
-
-void insertOld(std::string input){
 	std::ifstream file("cmudict.0.7a");
 	if (file.fail()){
 		std::cerr << "file not found\n";
@@ -260,54 +165,7 @@ void insertOld(std::string input){
 	}
 }
 
-bool checkRemove(std::string input, std::string pronunciation){
-	std::string before;
-	std::string after;
-	int words = numWords(input);
-	if (words != numWords(pronunciation)+1){
-		return false;
-	}
-	for (int i = 0; i < words; i++){
-		std::string pre0 = "", pre1 = "";
-		std::string before0, after0; //input
-		std::string before1, after1; //pronunciation
-		splitOnSpace(input,before0,after0); //first phoneme gone
-		splitOnSpace(pronunciation,before1,after1);
-		for (int j = 0; j < i; j++){ //remove other phonemes as necessary
-			pre0 = pre0 + before0 + " ";
-			splitOnSpace(after0,before0,after0);
-			splitOnSpace(after1,before1,after1);
-			pre1 = pre1 + before1 + " ";
-		}
-		if (!pre0.compare(pre1) && !after0.compare(after1)){
-			return true;
-		}
-	}
-	return false;
-}
-
 void remove(std::string input){
-	std::ifstream file("cmudict.0.7a");
-	if (file.fail()){
-		std::cerr << "file not found\n";
-		exit(1);
-	}
-	file.clear();
-	file.seekg(std::ios::beg);
-	std::string line;
-	while(std::getline(file,line)){
-		std::string word;
-		std::string pronunciation;
-		std::string junk;
-		splitOnSpace(line,word,pronunciation);
-		splitOnSpace(pronunciation,junk,pronunciation);//phonemes only, no space
-		if (checkRemove(input,pronunciation)){
-			std::cout << ' ' << word;
-		}
-	}
-}
-
-void removeOld(std::string input){
 	std::ifstream file("cmudict.0.7a");
 	if (file.fail()){
 		std::cerr << "file not found\n";
